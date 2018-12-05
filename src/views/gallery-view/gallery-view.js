6a54ab8aa1dcd5aa1c03a9f462c7cf80d9e7cf83
yhoshua ochoa <yhoshua8a@gmail.com>
2018-12-04 21:23:41 -0600

    layout views

--- src/views/gallery-view/gallery-view.js
+++ src/views/gallery-view/gallery-view.js
@@ -11,8 +11,38 @@
 
         static get properties() {
         return {
-          
+            routeData: {
+                observer: '_handleRoute'
+            },
+
+            dataHeroes: {
+                type: Array,
+                value: []
+            }
         };
+      }
+
+
+      async _handleRoute(newRoute, oldRoute) {
+        
+
+        if (newRoute.page === "gallery" && oldRoute) {
+            console.log("HERE", newRoute, oldRoute);
+            this.set('dataHeroes', []);
+
+            for (let i = 0; i < 20; i++) {
+                const id = Math.floor((Math.random() * 100)-1);
+                let dataHero = {};
+
+                await fetch(`https://superheroapi.com/api.php/2113315678698733/${id}/image`)
+                    .then((response) => response.json())
+                    .then((json) => {
+                        dataHero = Object.assign({}, json);
+                    })
+
+                    this.push('dataHeroes', dataHero);
+            }
+        }
       }
 
         
--- src/views/powerstats-view/powerstats-view.js
+++ src/views/powerstats-view/powerstats-view.js
@@ -12,9 +12,48 @@
 
         static get properties() {
         return {
-          
+            routeData: {
+                observer: '_handleRoute'
+            },
+
+            dataHeroes: {
+                type: Array,
+                value: []
+            }
         };
       }
+
+      async _handleRoute(newRoute, oldRoute) {
+        
+
+        if (newRoute.page === "powerstats" && oldRoute) {
+            console.log("HERE", newRoute, oldRoute);
+            this.set('dataHeroes', []);
+
+            for (let i = 0; i < 10; i++) {
+                const id = Math.floor(Math.random() * 100);
+                let dataHero = {};
+
+
+                await fetch(`https://superheroapi.com/api.php/2113315678698733/${id}/powerstats`)
+                    .then((response) => response.json())
+                    .then((json) => {
+                        dataHero = Object.assign({}, json);
+                    })
+
+                await fetch(`https://superheroapi.com/api.php/2113315678698733/${id}/image`)
+                    .then((response) => response.json())
+                    .then((json) => {
+                        dataHero = Object.assign(dataHero, json);
+                    })
+
+                    this.push('dataHeroes', dataHero);
+
+                    console.log("DATA",this.dataHeroes);
+            }
+        }
+    }
+
 
         
     }
